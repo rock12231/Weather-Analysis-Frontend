6457767b0c4b668ceefabb65afe85a86997b02c0
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-09-30 14:37:42 +0530

    componet added head foot home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeadComponent } from './head/head.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
